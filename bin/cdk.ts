54a10f67748cb4b3f0651e80303a083e13fe677a
claire qian <chenyun.qian@outlook.com>
2025-05-30 10:58:45 +0800

    feat: add basic authorization for import api

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,9 +2,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { ProductServiceStack } from '../lib/stacks/product-service-stack';
 import { ImportServiceStack } from '../lib/stacks/import-service-stack';
+import { AuthorizationServiceStack } from '../lib/stacks/authorization-service-stack';
 
 const app = new cdk.App();
 
+const authStack = new AuthorizationServiceStack(app, 'AuthorizationServiceStack', {
+  env: {
+    region: 'us-east-1',
+  },
+});
 const productStack = new ProductServiceStack(app, 'ProductServiceStack', {
   env: {
     region: 'us-east-1',
@@ -12,9 +18,13 @@ const productStack = new ProductServiceStack(app, 'ProductServiceStack', {
   description: 'EPAM CloudX Serverless API Assignment Stack',
 });
 
-new ImportServiceStack(app, 'ImportServiceStack', {
+const importStack = new ImportServiceStack(app, 'ImportServiceStack', {
   catalogItemsQueue: productStack.catalogItemsQueue,
   env: {
     region: 'us-east-1',
   },
+  // basicAuthorizer: authStack.basicAuthorizer,
+  authorizerLambdaArn: authStack.basicAuthorizer.functionArn,
 });
+
+importStack.addDependency(authStack);
---
+++ lib/stacks/authorization-service-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({ path: path.join(__dirname, '../lambda/authorization-service/.env') });
+
+export class AuthorizationServiceStack extends cdk.Stack {
+  public readonly basicAuthorizer: lambda.Function;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 从 .env 加载凭据
+    const credentials = process.env;
+    const envVariables: { [key: string]: string } = {};
+    for (const [key, value] of Object.entries(credentials)) {
+      if (value) envVariables[key] = value;
+    }
+
+    // 创建 Lambda
+    this.basicAuthorizer = new lambda.Function(this, 'BasicAuthorizer', {
+      functionName: 'BasicAuthorizerFunction', // 显式设置物理名称
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'basic-authorizer.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/authorization-service')),
+      environment: envVariables,
+      timeout: cdk.Duration.seconds(30),
+    });
+
+    new cdk.CfnOutput(this, 'AuthorizerArn', {
+      value: this.basicAuthorizer.functionArn,
+    });
+  }
+}
